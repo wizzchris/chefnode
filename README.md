49dae295a97f831d96c6f71ce41603ed15a3509a
Christopher Rogers <CRogers@spartaglobal.com>
2020-03-09 12:58:41 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# node_cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: node_cookbook
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
